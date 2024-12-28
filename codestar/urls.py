48a76c46bb503a332a70565e080cd83781f70628
Sean Boland <boland1919@gmail.com>
2024-12-28 22:22:24 +0000

    readies code for deploy

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import views as blog_views
+from blog import views as blog_views
 
 urlpatterns = [
     path("blog/", blog_views.my_blog, name='blog'),
